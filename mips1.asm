41e8e01c50ba5f6b5139a7093b146cd4e2784c92
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-06-06 15:55:36 -0300

    arruma commit

--- mips1.asm
+++ mips1.asm
@@ -2,25 +2,13 @@
 .text
 .globl main
 main:
-	li $a0,0                        # inicia o total
-        li $a1,1                        # inicia o contador
-Loop:
-	add $a0,$a0,$a1                # adiciona o contador no total
-        addi $a1,$a1,1                # incrementa o contador
-        jal scanf
-        ble $a1,2,Loop                #verifica se terminou e faz o loop
-        j fim
-scanf:
-	li $v0, 4 #syscall de print string
-        la $a0, prompt #conteudo para printar
+        la $a0,$texto                # imprime texto "Hello World"
+        li $v0,4
         syscall
-	li $v0, 5 #syscall para ler inteiros
-        syscall
-        move $t0, $v0 #coloca valor no registrador temporario
-        jr $ra
         
 fim:
 	li $v0,10                        # termina
         syscall
 .data
-prompt: .asciiz "Digite um número inteiro: "
+$texto:
+        .asciiz "Hello World"
